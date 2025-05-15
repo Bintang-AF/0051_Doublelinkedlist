94128dd5bbf55d3ec31c4b391bec56e5c07ccf2b
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-05-15 11:22:24 +0700

    membuat void hapus

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -71,6 +71,35 @@ class doublelinkedlist
         newnode->next = current->next;
         newnode->prev = current;
 
+        if (current -> next != NULL)
+            current ->next -> prev = newnode; 
+
+
+
+    }
+
+    void hapus()
+    {
+        if (START == NULL){
+            cout << "\nList is empty"<< endl;
+            return ;
+
+        }
+
+        cout << "\nEnter the roll number of the student whose record is to be deleted : ";
+        int rollNo;
+        cin >> rollNo;
+
+        node *current = START;
+
+        while (current != NULL && current -> noMhs != rollNo)
+            current = current->next;
+        
+        if (current == NULL)
+        {
+            cout << "Record not found " << endl;
+            return;
+        }
     }
 
 }
