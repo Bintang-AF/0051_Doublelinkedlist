e21001e2e14e2ffbae3b3ee494552e6412a0afe9
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-05-15 10:29:11 +0700

    menambahkan modifier pada variabel

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -5,7 +5,27 @@ using namespace std;
 class node 
 {
     public:
-        int nomMhs;
+        int noMhs;
         node *next;
         node *prev;
 };
+
+class doublelinkedlist
+{
+    private :
+        node *START;
+        
+    public :
+        doublelinkedlist()
+        {
+            START = NULL;
+        }
+
+    void addnote()
+    {
+        int nim;
+        string nm;
+        cout << "\nEnter the roll number of the student";
+        cin >> nim;
+    }
+}
